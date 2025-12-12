6cb4c283176070fd69bed0821dc96059bfabaaa2
Donald-Edianm <kobbyedinamdonald@gmail.com>
2025-12-12 13:12:21 +0000

    Chore: Used image from pexels

--- components/footer.tsx
+++ components/footer.tsx
@@ -89,7 +89,7 @@ const Footer = () => {
               loop
               playsInline
             >
-              <source src="https://videos.pexels.com/video-files/6894223/6894223-uhd_2560_1440_25fps.mp4" type="video/mp4" />
+              <source src="https://videos.pexels.com/video-files/10537262/10537262-uhd_2732_1440_25fps.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
 
